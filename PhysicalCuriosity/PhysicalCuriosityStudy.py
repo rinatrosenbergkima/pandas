aee1194fe83eca5b2b3aedbec932760df2bc6093
rinatrosenberg-kima <rinat.rosenberg.kima@gmail.com>
2017-11-09 13:04:02 +0200

    Rinat installed pandas

---
+++ PhysicalCuriosity/PhysicalCuriosityStudy.py
@@ -0,0 +1,15 @@
+#tutorial from Tor:
+#https://github.com/QuantScientist/Deep-Learning-Boot-Camp/blob/master/day01/Intro_ml_models.ipynb
+#https://github.com/QuantScientist/Deep-Learning-Boot-Camp/blob/master/day01/predicting_income_from_census_income_data.ipynb
+
+
+# > sudo easy_install pip
+# > pip install wheel
+# > pip install pandas
+
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
