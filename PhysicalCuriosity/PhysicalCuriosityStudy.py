d21e86751236d0ad4a530f44c13200538f9e70b2
rinatrosenberg-kima <rinat.rosenberg.kima@gmail.com>
2017-12-13 15:23:03 +0200

    Rinat reads csv

--- PhysicalCuriosity/PhysicalCuriosityStudy.py
+++ PhysicalCuriosity/PhysicalCuriosityStudy.py
@@ -3,13 +3,23 @@
 #https://github.com/QuantScientist/Deep-Learning-Boot-Camp/blob/master/day01/predicting_income_from_census_income_data.ipynb
 
 
+# > this is how to install pandas
 # > sudo easy_install pip
 # > pip install wheel
 # > pip install pandas
-
 
 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+import matplotlib as mpl
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cbook as cbook
+
+df=pd.read_csv('data/AQ.csv', sep=',',header=None)
+df.values
+
+
+print(df.values)
